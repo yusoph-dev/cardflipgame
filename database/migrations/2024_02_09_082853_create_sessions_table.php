680515175253cc16a163eb4e37d68cfdf98e842e
Yusoph Suma <yusophh@gmail.com>
2024-02-13 11:05:01 +0800

    update: ui

--- database/migrations/2024_02_09_082853_create_sessions_table.php
+++ database/migrations/2024_02_09_082853_create_sessions_table.php
@@ -19,10 +19,10 @@ return new class extends Migration
             $table->bigInteger('count')->default(0);
             $table->boolean('active')->comment('active card')->default(false);
             $table->smallInteger('difficulty')->nullable();
-            $table->json('cards')->nullable();
-            $table->json('random_cards')->nullable();
+            $table->text('cards')->nullable();
+            $table->text('random_cards')->nullable();
             $table->smallInteger('matched_cards')->nullable();
-            $table->json('first_card')->nullable();
+            $table->text('first_card')->nullable();
             $table->boolean('verifying')->default(false);
             $table->string('player_name')->nullable();
             $table->bigInteger('timer_seconds')->nullable();
